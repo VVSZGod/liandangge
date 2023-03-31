d035673792c3a620d3eeadf1dd867b54774b002e
ZhuBaiZhou <baizhou.zhu@jiamiantech.com>
2023-03-31 16:36:41 +0800

    提交修改参数

--- playground/src/main/java/com/jiamian/translation/config/WxWebMvcConfiguration.java
+++ playground/src/main/java/com/jiamian/translation/config/WxWebMvcConfiguration.java
@@ -2,7 +2,10 @@ package com.jiamian.translation.config;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +17,12 @@ public class WxWebMvcConfiguration implements WebMvcConfigurer {
 	public void addArgumentResolvers(
 			List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(new LoginUserHandlerMethodArgumentResolver());
+	}
+
+	@Bean
+	public HttpMessageConverters customConverters() {
+		MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MyHttpMessageConverter();
+		mappingJackson2HttpMessageConverter.setPrettyPrint(true);
+		return new HttpMessageConverters(mappingJackson2HttpMessageConverter);
 	}
 }
