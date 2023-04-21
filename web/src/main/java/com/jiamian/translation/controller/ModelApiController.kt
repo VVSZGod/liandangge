dd22c09c0355e2253cd9db822529bcb7fc7c0b9f
DingGuangHui <guanghui.ding@jiamiantech.com>
2023-04-21 12:02:10 +0800

    修改页码

--- web/src/main/java/com/jiamian/translation/controller/ModelApiController.kt
+++ web/src/main/java/com/jiamian/translation/controller/ModelApiController.kt
@@ -1,10 +1,10 @@
 package com.jiamian.translation.controller
 
 import com.alibaba.fastjson.JSONObject
+import com.jiamian.translation.exception.BOException
 import com.jiamian.translation.service.ModelApiService
 import io.swagger.annotations.Api
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -25,7 +25,10 @@ open class ModelApiController {
 
 
     @GetMapping("/models")
-     open fun pageModel(@RequestParam(value = "page") page: Int, @RequestParam(value = "limit") pageSize: Int): String {
+    open fun pageModel(@RequestParam(value = "page") page: Int, @RequestParam(value = "limit") pageSize: Int): String {
+        if (pageSize > 100) {
+            throw BOException("max pageSize limit 100")
+        }
         return JSONObject.toJSONString(modelApiService.pageModelApi(pageSize, page))
     }
 }
